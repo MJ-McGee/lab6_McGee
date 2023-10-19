63f259f44695246892c32addbabdcc50a9259691
BATTLESTATION1\darth <darthmj03@live.com>
2023-10-19 18:58:06 -0500

    Implemented printing of matrices A and B

--- square_matrix.cpp
+++ square_matrix.cpp
@@ -2,34 +2,73 @@
 Author: MJ McGee
 Date Created: 10/19/23
 Date Last Modified: 10/19/23
+Source: stackoverflow.com
 Description: Read an input file to get 2 square matrices, print them, print their sum, print their dot product, and print their difference.
 */
 #include <iostream>
 #include <fstream>
+#include <stdint.h>
 using namespace std;
 
 //Declare the maximum square matrix size as a constant global variable
 const int N = 100;
 
 //Declare function prototypes
-void print_matrix(int[N][N]);
-void sum_matrices(int[N][N], int[N][N], int[N][N]);
-void dot_product_matrices(int[N][N], int[N][N], int[N][N]);
-void difference_matrices(int[N][N], int[N][N], int[N][N]);
+void print_matrix(int A[N][N], int n);
+void sum_matrices(int A[N][N], int B[N][N], int C[N][N], int n);
+void dot_product_matrices(int A[N][N], int B[N][N], int C[N][N], int n);
+void difference_matrices(int A[N][N], int B[N][N], int C[N][N], int n);
 
 //Driver function
 int main() {
-    char x;
+    //Initialize variables
+    int temp;
+    int n;
     fstream file("matrix_input.txt");
+    int A[N][N];
+    int B[N][N];
+    int C[N][N];
+    //Set the size of the matrices
+    file >> n;
 
-    for(int i = 0; i< 64; i++) {
-        file >> x;
-        cout << x;
+    if(n <= N) {
+    //Print name and lab number
+        cout << "MJ McGee\nLab #6: Matrix Multiplication\n" << endl;
+        
+        //Build two 2D arrays by getting values from the file
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                file >> temp;
+                A[i][j] = temp;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                file >> temp;
+                B[i][j] = temp;
+            }
+        }
+
+        //Print out A and B
+        cout << "Matrix A:" << endl;
+        print_matrix(A, n);
+
+        cout << "\nMatrix B:" << endl;
+        print_matrix(B, n);
     }
+    else {
+        cout << n << "x" << n << " is too large. Please enter a smaller matrix size!" << endl;
+    }
+    cout << "\nTERMINATING PROGRAM..." << endl;
     return 0;
 }
 
 //Declare functions
-void sum_matrices(int[N][N], int[N][N], int[N][N]) {
-    return;
+void print_matrix(int A[N][N], int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
